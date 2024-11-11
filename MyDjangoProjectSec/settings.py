9cf18d98b8b62e2cd73be2a3cf9a5e768cb373ae
Bulent Morten <bbmorten@gmail.com>
2024-11-11 11:39:50 +0300

    MyDjangoProjectSec

--- MyDjangoProjectSec/settings.py
+++ MyDjangoProjectSec/settings.py
@@ -6,7 +6,7 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
 
 SECRET_KEY = 'django-insecure-a&*o#8@w&8f7en!k7pau5m2ohgq%r@=bg0xq^1-wn!bsvbxl)3'
 DEBUG = True
-ALLOWED_HOSTS = [mydjangoprojectsec.dvmo.uk]
+ALLOWED_HOSTS = [MyDjangoProjectSec.dvmo.uk]
 
 
 # Application definition
